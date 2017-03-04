ba1fc3e852cc4ced87b76ad0dbc8996b5ae8fbb1
Mori <iwona.moryc92@gmail.com>
2017-03-04 12:09:43 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,6 +1,6 @@
 <?php
 
-if($_POST["submit"]) {
+if($_POST["contact_submit_btn"]) {
     $recipient="damianpasko92@gmail.com";
     $subject="Form to email message";
     $sender=$_POST["contact_name"];
