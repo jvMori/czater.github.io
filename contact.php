f89c1418fea8071d4b74fd46d5c33cfc01d65a9c
Mori <iwona.moryc92@gmail.com>
2017-03-04 11:52:47 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,3 +1,15 @@
 <?php
-echo "The message has been sent!";
-?>
+
+if($_POST["submit"]) {
+    $recipient="damianpasko92@gmail.com";
+    $subject="Form to email message";
+    $sender=$_POST["contact_name"];
+    $senderEmail=$_POST["contact_email"];
+    $message=$_POST["contact_message"];
+
+    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+
+    $thankYou="<p>Thank you! Your message has been sent.</p>";
+}
